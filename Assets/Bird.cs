ace3278da5bb37b2f14afad7f8e16ad2cd7c70b5
cwx2810 <cwx2810@163.com>
2017-10-04 09:13:51 -0700

    添加小鸟原地动画

---
+++ Assets/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	public float timer;
+	public int frameNumber = 10;//frame number per second
+	public int frameCount = 0;//frame counter
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= 1.0f / frameNumber) {
+			frameCount++;
+			timer -= 1.0f / frameNumber;
+
+			//update matirial's offset x while frame count ++
+			int frameIndex = frameCount % 3;
+			this.GetComponent<Renderer>().material.SetTextureOffset ("_MainTex", new Vector2 (0.33f * frameIndex, 0));
+		}
+	}
+}
