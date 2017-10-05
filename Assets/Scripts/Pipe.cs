14f9c3926879de7dc6c790d831d2fc5b4a48f726
cwx2810 <cwx2810@163.com>
2017-10-05 08:43:29 -0700

    完成钢管位置随机生成，小鸟自动下落

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour {
+
+	void Start(){
+		RandomGeneratePosition ();
+	}
+
+	public void RandomGeneratePosition(){
+		float pos_y = Random.Range (-0.2f, 0.1f);
+		this.transform.localPosition = new Vector3 (this.transform.localPosition.x, pos_y, this.transform.localPosition.z);
+	}
+}
