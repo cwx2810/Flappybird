ecf768c5f0cca9e805e9ddc00761e21e633994ac
cwx2810 <cwx2810@163.com>
2017-10-08 17:46:02 -0700

    更新新地图管道位置生成

--- Assets/MoveTrigger.cs
+++ Assets/MoveTrigger.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class MoveTrigger : MonoBehaviour {
 
 	public Transform currentBg;
+	public Pipe pipe1;
+	public Pipe pipe2;
 
 	public void OnTriggerEnter (Collider other) {
-		if (other.tag == "player") {
+		if (other.tag == "Player") {
 			//load 3rd Bg
 			Transform firstBg = GameManager._instance.firstBg;
 			//update currentBg's position after 3rd Bg
 			currentBg.position = new Vector3 (firstBg.position.x+10,currentBg.position.y,currentBg.position.z);
 			//update 3rd Bg
 			GameManager._instance.firstBg = currentBg;
+
+			//update new bg pipe's position
+			pipe1.RandomGeneratePosition ();
+			pipe2.RandomGeneratePosition ();
 		}
 	}
 }
