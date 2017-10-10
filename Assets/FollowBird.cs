4c29c8211dfd4084ed74310edfd08f19f7615750
cwx2810 <cwx2810@163.com>
2017-10-10 08:52:00 -0700

    手写相机跟随

---
+++ Assets/FollowBird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBird : MonoBehaviour {
+
+	private GameObject bird;
+	private Transform birdTransform;
+
+	// Use this for initialization
+	void Start () {
+		bird = GameObject.FindGameObjectWithTag ("Player");
+		birdTransform = bird.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 birdPos = birdTransform.position;
+		this.transform.position = new Vector3 (birdPos.x+1.23f, birdPos.y-2, birdPos.z-23.04f);
+	}
+}
